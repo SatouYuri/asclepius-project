18e58c3d445d53441594a864e44330686a289fcc
Acer <Acer@177.220.84.31>
2019-04-18 19:26:27 -0300

    CONSERTEI A MERDA DA CLASSE QUE O SANTANCHÉ FEZ. (DataSetComponent)

--- src/asclepius/Asclepius.java
+++ src/asclepius/Asclepius.java
@@ -1,7 +1,27 @@
 package asclepius;
 
+import asclepius.DSC.DataSetComponent;
+import asclepius.DSC.IDataSet;
+        
 public class Asclepius {
     public static void main(String[] args) {
-        System.out.println("The World, pare o tempo!");
+        System.out.println("THE WORLD! Pare o tempo agora!");
+
+        IDataSet DS = new DataSetComponent();
+        DS.setDataSource("src\\data\\test-cases.csv");
+        String attributes[] = DS.requestAttributes();
+        for(int a = 0; a < (attributes.length) - 1; a++){
+            System.out.print(attributes[a] + ", ");
+        }
+        System.out.println(attributes[(attributes.length) - 1]);
+        
+        System.out.println();
+        String instances[][] = DS.requestInstances();
+        for(int i = 0; i < instances.length; i++){
+            for(int a = 0; a < (attributes.length) - 1; a++){
+                System.out.print(instances[i][a] + ", ");
+            }
+            System.out.println(instances[i][(attributes.length) - 1]);
+        }
     }
 }
--- src/asclepius/DSC/DataSetComponent.java
+++ src/asclepius/DSC/DataSetComponent.java
@@ -1,65 +1,69 @@
 package asclepius.DSC;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class DataSetComponent implements IDataSet{
-  private String dataSource = null;
-  private String[] attributes = null;
-  private String[][] instances = null;
+public class DataSetComponent implements IDataSet {
+    private String dataSource = null;
+    private String[] attributes = null;
+    private String[][] instances = null;
   
-  public DataSetComponent() {
-    /* nothing */
-  }
-
-  @Override
-  public String getDataSource() {
-    return dataSource;
-  }
-
-  @Override
-  public void setDataSource(String dataSource) {
-    this.dataSource = dataSource;
-    if (dataSource == null) {
-      attributes = null;
-      instances = null;
-    } else
-      readDS();
-  }
-  
-    /**
-     *
-     * @return
-     */
-    @Override
-  public String[] requestAttributes() {
-    return attributes;
-  }
-  
-  @Override
-  public String[][] requestInstances() {
-    return instances;
-  }
-  
-  private void readDS() {
-    ArrayList<String[]> instArray = new ArrayList<>();
-    try {
-        try (BufferedReader file = new BufferedReader(new FileReader(dataSource))) {
-            String line = file.readLine();
-            if (line != null) {
-                attributes = line.split(",");
-                line = file.readLine();
-                while (line != null) {
-                    String[] instLine = line.split(",");
-                    instArray.add(instLine);
-                    line = file.readLine();
-                }
-                instances = instArray.toArray(new String[0][]);
-            } }
-    } catch (IOException erro) {
+    public DataSetComponent(){
     }
-  }
+
+    @Override
+    public String getDataSource(){
+        return dataSource;
+    }
+
+    @Override
+    public void setDataSource(String dataSource){
+        this.dataSource = dataSource;
+        if (dataSource == null) {
+            attributes = null;
+            instances = null;
+        }else{
+            try {
+                readDS();
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(DataSetComponent.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(DataSetComponent.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
   
+    @Override
+    public String[] requestAttributes(){
+        return attributes;
+    }
+  
+    @Override
+    public String[][] requestInstances(){
+        return instances;
+    }
+
+    private void readDS() throws FileNotFoundException, IOException{
+        ArrayList<String[]> instArray = new ArrayList<>();
+        BufferedReader file = new BufferedReader(new FileReader(dataSource));
+        String line = file.readLine();
+        
+        if(line != null){
+            attributes = line.split(",");
+            line = file.readLine();
+            while(line != null){
+                String[] instLine = line.split(",");
+                instArray.add(instLine);
+                line = file.readLine();
+            }
+            instances = instArray.toArray(new String[0][]);
+        }
+        
+        file.close();
+    }
 }
