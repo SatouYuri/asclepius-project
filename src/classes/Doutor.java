4b8d2c103a52baa2c57a1663a33fe3e9a5fddd2f
maril <maril@177.220.85.111>
2019-04-18 15:48:06 -0300

    Adicionando classes Doutor e Paciente mais as interfaces necessárias.

---
+++ src/classes/Doutor.java
@@ -0,0 +1,21 @@
+package classes;
+
+import interfaces.IEnquirer;
+import interfaces.IResponder;
+import interfaces.IResponderReceptacle;
+
+public class Doutor implements IResponderReceptacle, IEnquirer{
+
+	@Override
+	public void startInterview() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void connect(IResponder responder) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/classes/Paciente.java
@@ -0,0 +1,19 @@
+package classes;
+
+import interfaces.IResponder;
+
+public class Paciente implements IResponder{
+
+	@Override
+	public String ask(String question) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean finalAnswer(String answer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
