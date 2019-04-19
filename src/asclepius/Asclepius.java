62ec5ecee590fe4e5ccbb258cdd5e31cd4183c8b
Acer <Acer@177.220.84.31>
2019-04-18 23:51:03 -0300

    DataSetComponent começou a ser implementado no código principal.

--- src/asclepius/Asclepius.java
+++ src/asclepius/Asclepius.java
@@ -6,5 +6,16 @@ import asclepius.DSC.IDataSet;
 public class Asclepius{
     public static void main(String[] args) {
         System.out.println("THE WORLD! Pare o tempo agora!");
+        
+        IDataSet DS = new DataSetComponent();
+        DS.setDataSource("src\\data\\test-cases.csv");
+        String attributes[] = DS.requestAttributes();
+        String instances[][] = DS.requestInstances();
+        for(int i = 0; i < instances.length; i++){
+            for(int a = 0; a < (attributes.length) - 1; a++){
+                System.out.print(instances[i][a] + ", ");
+            }
+            System.out.println(instances[i][(attributes.length) - 1]);
+        }
     }
 }
