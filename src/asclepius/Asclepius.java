52bbd4242abbb4b19506662f341da361a2310741
SatouYuri <>
2019-04-20 15:33:30 -0300

    O método ask() da classe Paciente está pronto para uso. Um teste na nossa classe principal, "Asclepius", foi inserido temporariamente para que os outros membros do projeto possam verificar o funcionamento.

--- src/asclepius/Asclepius.java
+++ src/asclepius/Asclepius.java
@@ -2,15 +2,27 @@ package asclepius;
 
 import asclepius.DSC.DataSetComponent;
 import asclepius.DSC.IDataSet;
+import classes.Paciente;
+import interfaces.IResponder;
         
 public class Asclepius{
     public static void main(String[] args) {
         System.out.println("THE WORLD! Pare o tempo agora!");
         
+        /*Capturando informação do acervo de dados .CSV*/
         IDataSet DS = new DataSetComponent();
         DS.setDataSource("src\\data\\test-cases.csv");
         String attributes[] = DS.requestAttributes();
         String instances[][] = DS.requestInstances();
         
+        /*Testando o método ask() da classe Paciente*/
+        IResponder P1 = new Paciente();
+        IResponder P2 = new Paciente();
+        
+        System.out.println(P1.ask("paralysis"));
+        System.out.println(P2.ask("paralysis"));
+        
+        System.out.println(P1.ask("gordice"));
+        System.out.println(P1.ask("dor_na_alma"));
     }
 }
--- src/classes/Paciente.java
+++ src/classes/Paciente.java
@@ -1,12 +1,61 @@
 package classes;
 
+import asclepius.DSC.DataSetComponent;
+import asclepius.DSC.IDataSet;
 import interfaces.IResponder;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Paciente implements IResponder{   
+    /*Esse ArrayList vai guardar os 7 sintomas (yes/no), os dados do Paciente*/
+    private ArrayList<String> dataList = new ArrayList<>();
+    
+    public Paciente(){
+    /*Quando um Paciente for instanciado, ele obrigatória e aleatoriamente recebe um conjunto de dados diretamente da base .CSV*/
+        IDataSet DS = new DataSetComponent();
+        DS.setDataSource("src\\data\\test-cases.csv");
+        String attributes[] = DS.requestAttributes();
+        String instances[][] = DS.requestInstances();
+        int randomData = new Random().nextInt(instances.length);
+        for(int x = 0; x < 7; x++){    
+            dataList.add(instances[randomData][x]);
+        }
+        //TESTE OU DEBUGGING
+        /*Habilite essa parte do código para fins de teste*/
+        for(int x = 0; x < dataList.size(); x++){    
+            System.out.printf("%s ", dataList.get(x));
+        }
+        System.out.println();
+        //TESTE OU DEBUGGING
+    }
+    
     @Override 
     public String ask(String question) {
+        /*Chamando um DS para adquirir o vetor de sintomas*/
+        IDataSet DS = new DataSetComponent();
+        DS.setDataSource("src\\data\\test-cases.csv");
+        String attributes[] = DS.requestAttributes();
         
-        return null;
+        /*Checando se o sintoma existe e gerando a posição counter do vetor que possui tal sintoma, se existit.*/
+        int counter = 0;
+        for(int x = 0; x < attributes.length - 1; x++){
+            if(attributes[x].equalsIgnoreCase(question)){
+                break;
+            }
+            counter++;
+        }
+        
+        if(counter < 7){ //Se o sintoma existe...
+            if(dataList.get(counter).equalsIgnoreCase("t")){
+                return "yes";
+            }else if(dataList.get(counter).equalsIgnoreCase("f")){
+                return "no";
+            }
+        }else if(counter == 7){ //Se o sintoma não existe...
+            return "unknown";
+        }
+        
+        return "Function ask() has crashed.";
     }
 
     @Override
