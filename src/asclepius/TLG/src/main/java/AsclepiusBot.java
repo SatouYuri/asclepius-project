da2d03124294592082581ea23db3e67cdd69a121
francisconavy <francisconavy@yahoo.com.br>
2019-05-28 15:50:53 -0300

    cumprimento feito

--- src/asclepius/TLG/src/main/java/AsclepiusBot.java
+++ src/asclepius/TLG/src/main/java/AsclepiusBot.java
@@ -1,7 +1,9 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -12,38 +14,27 @@ public class AsclepiusBot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
 
         String text = update.getMessage().getText();
-        if(text.equals("/start")){
-            String nome = update.getMessage().getFrom().getFirstName();
-            SendMessage message = new SendMessage();
-            message.setText("Olá "+nome+"! Vamos iniciar a consulta?\n");
-            message.setChatId(update.getMessage().getChatId());
-            try {
-                execute(message);
-                //System.out.println(text);
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
-        }
-        System.out.println(update.getMessage().getText());
-/*
-        String text = update.getMessage().getText();
-        //System.out.println(text);
-
         String nome = update.getMessage().getFrom().getFirstName();
+        if(text.equals("/start")){
+            sendText(update, "Olá, "+nome+"! Vamos iniciar a consulta?");
+
+        }
+
+
+        System.out.println(update.getMessage().getText());
+
+    }
+
+    public void sendText(Update update, String text){
         SendMessage message = new SendMessage();
-        message.setText(nome+": "+ text +"\n");
+        message.setText(text+"\n");
         message.setChatId(update.getMessage().getChatId());
         try {
             execute(message);
-            //System.out.println(text);
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
-
-        System.out.println(text);
-
- */
-
+        return;
     }
 
     public String getBotUsername() {
